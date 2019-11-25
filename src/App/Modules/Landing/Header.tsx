da7cfe12cc237b5ffcff9d6bafada6e5023edf5d
hoangbaovu <baovuhbv@gmail.com>
2019-11-25 22:07:01 +0700

    Added Banner

--- src/App/Modules/Landing/Header.tsx
+++ src/App/Modules/Landing/Header.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Navbar from './Navbar';
 import './less/Header.less';
 
--- src/App/Modules/Landing/index.tsx
+++ src/App/Modules/Landing/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Row, Col } from 'antd';
 import LandingHeader from './Header';
+import LandingBanner from './Banner';
 const { Content, Footer } = Layout;
 
-const Landing: React.FC = () => (
-  <>
-    <Layout>
-      <LandingHeader />
-      <Content style={{ margin: '24px 16px 0' }}>
-        <div>content</div>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>©2019 Made with &lt;3 <a href="//fb.com/baobinhaquarius" target="_blank" rel="noopener noreferrer">Bảo Bình - Aquarius</a></Footer>
-    </Layout>
-  </>
+const Landing = () => (
+  <Layout className="layout">
+    <LandingHeader />
+    <LandingBanner />
+    <Content style={{ margin: '24px 16px 0' }}>
+      <div className="container">
+      </div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>©2019 Made with &lt;3 <a href="//fb.com/baobinhaquarius" target="_blank" rel="noopener noreferrer">Bảo Bình - Aquarius</a></Footer>
+  </Layout>
 );
 
 export default Landing;
