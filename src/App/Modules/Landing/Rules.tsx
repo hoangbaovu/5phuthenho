7af8f33c5d4392f3db396079f416e1170e7e30bc
hoangbaovu <baovuhbv@gmail.com>
2019-11-28 00:12:35 +0700

    Added Navbar active

--- src/App/Modules/Landing/Rules.tsx
+++ src/App/Modules/Landing/Rules.tsx
@@ -14,10 +14,11 @@ const LandingRules = ({ id }: Props) => {
         <p className="Rules__content">
           Đơn giản là sẽ có 20 bạn nam và 20 bạn nữ cùng tham gia, mỗi cặp sẽ có 5 phút để trò chuyện với nhau nhưng không được phép lộ thông tin cá nhân của mình (chỉ được gọi nhau bằng số gắn trên áo).
           Hết 5 phút chuyện trò, bạn nữ ngồi im tại chỗ còn bạn nam đứng lên di chuyển sang bàn bên cạnh.
-          Vậy thì quá tuyệt khi 1 buổi chiều được gặp gỡ tận 20 người khác giới, trò chuyện và làm quen cùng họ.
+          Vậy thì quá tuyệt khi một buổi chiều được gặp gỡ tận 20 người khác giới, trò chuyện và làm quen cùng họ.
           Và còn tuyệt hơn nữa nếu cô gái hay chàng trai bạn thích cũng chọn bạn ở cuối chương trình!!!
           Chúc mừng bạn đã có một mùa đông không lạnh!!!
         </p>
+        <img className="Rules__pose" src={require("../../../assets/pose_puzzle_kumiawaseru.png")} alt=""/>
       </div>
     </div>
   )
