b2c95410258d481c6686d70327d4880d5b3fb61e
hoangbaovu <baovuhbv@gmail.com>
2019-12-15 11:20:07 +0700

    Added background guest

--- src/components/Landing/Guests.tsx
+++ src/components/Landing/Guests.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Row } from 'antd';
 import ScrollOverPack from 'rc-scroll-anim/lib/ScrollOverPack';
+import styled from 'styled-components';
 import QueueAnim from 'rc-queue-anim';
 import { HeaderTitle } from '../../shared/HeaderTitle';
 import LandingGuestsInfo from './GuestsInfo';
-import './less/Guests.less';
 
 type Props = {
   data: any,
   id: string,
 }
+
+const Guests = styled.div`
+  background: #fef8f8 url(../../assets/bg_intro.jpg) no-repeat top;
+  padding: 100px 0;
+  min-height: 100vh;
+`;
 
 const LandingGuests = ({ data, id }: Props) => {
 
@@ -18,7 +24,7 @@ const LandingGuests = ({ data, id }: Props) => {
   });
 
   return (
-    <ScrollOverPack id={id} className="Guests">
+    <ScrollOverPack id={id} component={Guests}>
       <QueueAnim
         className="container"
         key="text"
